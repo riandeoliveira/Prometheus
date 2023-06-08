d1296a9fe59062d68ed78002d4e8c3352b37848f
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-06-08 20:55:04 -0300

    feat: improve TemplateRepository

--- new-plan/server/src/repositories/TemplateRepository.ts
+++ new-plan/server/src/repositories/TemplateRepository.ts
@@ -1,11 +1,16 @@
 interface ITemplateData {
   author: string;
-  backend: string;
+  backend: "Firebase" | "NextJS API";
   currentYear: number;
+  description: string;
+  email: string;
+  githubRepoName: string;
   githubUsername: string;
+  keywords: string[];
   lowerCaseProjectName: string;
   projectName: string;
-  stateManagement: string;
+  stateManagement: "Context API" | "Redux Toolkit" | "Zustand";
+  style: "CSS Modules" | "SASS Modules" | "Styled Components" | "TailwindCSS";
   technologies: string[];
 }
 
@@ -15,16 +20,30 @@ interface ITemplateRepository {
 
 export class TemplateRepository implements ITemplateRepository {
   public data: ITemplateData;
+  public ignoredFiles: string[];
 
   constructor() {
     this.data = {
       author: "Rian Oliveira",
-      backend: "asd",
+      backend: "Firebase",
       currentYear: new Date().getFullYear(),
+      description: "Este é meu projeto.",
+      email: "riandiasdeoliveira2001@gmail.com",
+      githubRepoName: "BinanceAPP",
       githubUsername: "riandeoliveira",
+      keywords: [
+        "context-api",
+        "css",
+        "firebase",
+        "javascript",
+        "nextjs",
+        "react",
+        "typescript",
+      ],
       lowerCaseProjectName: "binance_app",
       projectName: "BinanceAPP",
-      stateManagement: "context_api",
+      stateManagement: "Zustand",
+      style: "Styled Components",
       technologies: [
         "css",
         "firebase",
@@ -36,5 +55,28 @@ export class TemplateRepository implements ITemplateRepository {
         "ts",
       ],
     };
+    this.ignoredFiles = [];
+
+    this.handleIgnoredFiles();
   }
+
+  private handleIgnoredFiles = (): void => {
+    if (this.data.backend !== "Firebase") {
+      this.ignoredFiles.push("env.d.ts.ejs");
+      this.ignoredFiles.push("firebase.ts.ejs");
+    }
+
+    if (this.data.style !== "CSS Modules") {
+      this.ignoredFiles.push("styles.module.css.ejs");
+    }
+
+    if (this.data.style !== "SASS Modules") {
+      this.ignoredFiles.push(".stylelintrc.json.ejs");
+    }
+
+    if (this.data.style !== "TailwindCSS") {
+      this.ignoredFiles.push("postcss.config.js.ejs");
+      this.ignoredFiles.push("tailwind.config.js.ejs");
+    }
+  };
 }
