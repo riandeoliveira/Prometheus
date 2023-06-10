a9e0e03d12573cb01abaddc33fcbbcf2c3259c09
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-06-10 19:52:33 -0300

    refactor: delete old rust structure

--- src/modules/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod answers;
-pub mod choices;
-pub mod prompt;
-pub mod questions;
-pub mod terminal;
--- src/templates/3_1_2_3_3_1/src/styles/colors.ts
+++
@@ -1,7 +0,0 @@
-export const colors = {
-  black: "#000000",
-  blue: "#006aeb",
-  white: "#ffffff",
-
-  complementary: "#ee7752, #e73c7e, #23a6d5, #23d5ab",
-};
