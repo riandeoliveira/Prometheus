f4f5d429acf5ed067aa6017091b9352a90bdd058
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-06-10 20:58:39 -0300

    feat(laravel): add laravel api template

---
+++ server/src/templates/api/laravel/app/Http/Requests/ResetPasswordRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Rules\StrongPassword;
+use App\Traits\RequestValidation;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest {
+  use RequestValidation;
+
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize(): bool {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, mixed>
+   */
+  public function rules(): array {
+    return [
+      'password' => ['required', 'string', 'min:8', 'max:20', new StrongPassword(), 'confirmed'],
+      'password_confirmation' => ['required', 'string', 'min:8', 'max:20', new StrongPassword()],
+    ];
+  }
+}
