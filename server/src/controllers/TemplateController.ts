c0a9cf0254bf01f34a51c424e81fe508331ee954
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-06-19 22:02:29 -0300

    refactor: refact template controller

--- server/src/controllers/TemplateController.ts
+++ server/src/controllers/TemplateController.ts
@@ -5,30 +5,19 @@ import { TemplateService } from "../services/TemplateService";
 
 class TemplateController {
   public generate = async (request: Request, response: Response) => {
-    const data: TemplateData = request.body;
+    const templateData: TemplateData = request.body;
 
-    const templateRepository: TemplateRepository = new TemplateRepository(data);
-
-    const templateService: TemplateService = new TemplateService(
-      "./src/templates/web/react_nextjs",
-      `./public/${Date.now()}`,
-      templateRepository
-    );
+    const templateRepository = new TemplateRepository(templateData);
+    const templateService = new TemplateService(templateRepository);
 
     try {
-      templateService.copyToPublicDirectory();
+      const template: string = await templateService.generate();
 
-      await templateService.setPathList();
-
-      templateService.renderFileList();
-
-      const zipDirectoryPath: string = await templateService.zip();
-
-      return response.download(zipDirectoryPath);
+      return response.download(template);
     } catch (error: unknown) {
       return response.json(error);
     }
   };
 }
 
-export const templateController: TemplateController = new TemplateController();
+export const templateController = new TemplateController();
