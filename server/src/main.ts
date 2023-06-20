03b9d5af0cdb110883f5d837e4c33edac2871c44
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-06-20 19:37:46 -0300

    build: prepare to build api on vercel

--- server/src/main.ts
+++ server/src/main.ts
@@ -34,6 +34,6 @@ app.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("Server running!");
 });
